ca926641766485547c3ca7aed5f238a65ce44ab9
Raghavendra2211 <57110960+Raghavendra2211@users.noreply.github.com>
2019-10-29 00:32:08 +0530

    Create Assignment1_2.py

---
+++ Assignment1_2.py
@@ -0,0 +1,12 @@
+# Assignment no 2
+
+def ChkNum(number):
+	if number%2 == 0:
+		print("Output: Even Number")
+	else:
+		print("Output: Odd number")
+
+
+number=int(input("Input: "))
+
+ChkNum(number)
