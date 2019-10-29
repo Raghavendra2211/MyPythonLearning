ecb77c435725b82fbe7e2e76ef4f6329ad85af5b
Raghavendra2211 <57110960+Raghavendra2211@users.noreply.github.com>
2019-10-29 11:18:06 +0530

    Create Assignment1_6.py

---
+++ Assignment1_6.py
@@ -0,0 +1,13 @@
+# Assignment no 6
+
+def chkNum(number):
+	if number == 0:
+		print("Zero")
+	elif number > 0:
+		print("Positive Number")
+	else:
+		print("Negative Number")
+
+
+number=int(input("Enter any number: "))
+chkNum(number)
