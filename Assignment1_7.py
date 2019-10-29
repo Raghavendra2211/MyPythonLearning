e4428340e800c371dcb7e64351be736b5147771d
Raghavendra2211 <57110960+Raghavendra2211@users.noreply.github.com>
2019-10-29 11:24:50 +0530

    Create Assignment1_7.py

---
+++ Assignment1_7.py
@@ -0,0 +1,13 @@
+# Assignment no 7
+
+def checkDivison(number,divisor):
+	if number%divisor == 0:
+		print("True")
+	else:	
+		print("False")
+
+
+number=int(input("Input: "))
+checkDivison(number,5)
+	
+	
