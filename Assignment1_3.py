cb36ad2eceeb92f14e963dafb01936cb0f30af4a
Raghavendra2211 <57110960+Raghavendra2211@users.noreply.github.com>
2019-10-29 10:48:04 +0530

    Create Assignment1_3.py

---
+++ Assignment1_3.py
@@ -0,0 +1,12 @@
+# Assignment no 3
+
+def Add(number1,number2):
+	return number1+number2
+
+
+number1=int(input("Input first number: "))
+number2=int(input("Input second number: "))
+Sum=Add(number1,number2)
+print("Output:", Sum) # Addition of two numbers
+
+
