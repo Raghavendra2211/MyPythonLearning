df4af2ff50175e4767b20fbc42702581a4f385b8
Raghavendra2211 <57110960+Raghavendra2211@users.noreply.github.com>
2019-11-01 19:44:57 +0530

    Create Assignment2_1.py

---
+++ Assignment2/Assignment2_1.py
@@ -0,0 +1,17 @@
+#Assignment No 2_1 
+#Arithmetic module that contains arithmetic functions
+
+
+def Add(no1,no2):
+	return no1+no2
+
+def Sub(no1,no2):
+	return no1-no2
+
+def Mult(no1,no2):
+	return no1*no2
+
+def Div(no1,no2):
+	return no1/no2
+
+
